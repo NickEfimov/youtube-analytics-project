1f63141c0c853752389d999cd02d27705acd1107
nikita efimov <nieff777@gmail.com>
2023-10-23 19:04:40 +0400

    new class added

---
+++ src/video.py
@@ -0,0 +1,31 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        self.title = video_response['items'][0]['snippet']['title']
+        self.view = video_response['items'][0]['statistics']['viewCount']
+        self.like = video_response['items'][0]['statistics']['likeCount']
+        self.link = f'https://www.youtube.com/watch?v={video_id}'
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('youtubeAPIkey')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
