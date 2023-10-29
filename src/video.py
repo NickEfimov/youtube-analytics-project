631f48f372861971328f00322ae33aab06699a96
nikita efimov <nieff777@gmail.com>
2023-10-29 20:34:47 +0400

    broken id exception added

--- src/video.py
+++ src/video.py
@@ -3,15 +3,22 @@ from googleapiclient.discovery import build
 
 
 class Video:
+
     def __init__(self, video_id):
         self.video_id = video_id
-        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.view = video_response['items'][0]['statistics']['viewCount']
-        self.like = video_response['items'][0]['statistics']['likeCount']
-        self.link = f'https://www.youtube.com/watch?v={video_id}'
+        try:
+            video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.view = video_response['items'][0]['statistics']['viewCount']
+            self.like = video_response['items'][0]['statistics']['likeCount']
+            self.link = f'https://www.youtube.com/watch?v={video_id}'
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
